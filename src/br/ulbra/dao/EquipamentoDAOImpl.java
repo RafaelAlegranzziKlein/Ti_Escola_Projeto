2a7f343afb1b821ea548033e12695fafabd95edc
Rafael Alegranzzi Klein <rafaelklein2007@gmail.com>
2026-04-20 12:37:17 -0300

    Adiciondo equipamentoDAOImpl

--- src/br/ulbra/dao/EquipamentoDAOImpl.java
+++ src/br/ulbra/dao/EquipamentoDAOImpl.java
@@ -1,10 +1,115 @@
-
 package br.ulbra.dao;
 
 /**
  *
  * @author rafae
  */
-public class EquipamentoDAOImpl {
-    
+import br.ulbra.model.Equipamento;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EquipamentoDAOImpl implements EquipamentoDAO {
+
+    @Override
+    public void salvar(Equipamento equipamento) {
+        String sql = "INSERT INTO equipamentos (tag_patrimonio, sala) VALUES (?, ?)";
+
+        try (Connection conn = ConnectionFactory.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, equipamento.getSala());
+            stmt.setString(2, equipamento.getTag_patrimonio());
+
+            stmt.executeUpdate();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Equipamento> listar() {
+        String sql = "SELECT * FROM equipamentos";
+        List<Equipamento> lista = new ArrayList<>();
+
+        try (Connection conn = ConnectionFactory.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Equipamento e = new Equipamento(
+                        rs.getInt("id_equipamento"),
+                        rs.getString("tag_patrimonio"),
+                        rs.getString("sala")
+                );
+                lista.add(e);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return lista;
+
+    }
+
+    @Override
+    public Equipamento buscarPorId(int id_equipamento) {
+        String sql = "SELECT * FROM equipamentos WHERE id_equipamento = ?";
+
+        try (Connection conn = ConnectionFactory.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setLong(1, id_equipamento);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new Equipamento(
+                        rs.getInt("id_equipamento"),
+                        rs.getString("tag_patrimonio"),
+                        rs.getString("sala")
+                );
+            }
+
+        } catch (Exception c) {
+            throw new RuntimeException(c);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void atualizar(Equipamento equipamentos) {
+        String sql = "UPDATE equipamentos SET  WHERE id_equipamento = ?";
+
+        try (Connection conn = ConnectionFactory.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, equipamentos.getTag_patrimonio());
+            stmt.setString(2, equipamentos.getSala());
+            stmt.setInt(3, equipamentos.getId_equipamento());
+            
+
+            stmt.executeUpdate();
+
+        } catch (Exception c) {
+            throw new RuntimeException(c);
+        }
+    }
+
+    @Override
+    public void deletar(int id_equipamento) {
+        String sql = "DELETE FROM equipamentos WHERE id_equipamento = ?";
+
+        try (Connection conn = ConnectionFactory.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id_equipamento);
+            stmt.executeUpdate();
+
+        } catch (Exception c) {
+            throw new RuntimeException(c);
+        }
+    }
 }
